75f3626b6eb4ec767904443a1db08112697afe3a
barisonedavid0-blip <barisone.david.0@gmail.com>
2026-09-14 01:02:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='jarvis-v2';
-const ASSETS=['./','index.html','manifest.json','icon-180.png','icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='jarvis-dbj-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.json','earth.png','worldmap.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
