c42fa3f3082a5bdb438aa59c0c32f47e375ec4b2
barisonedavid0-blip <barisone.david.0@gmail.com>
2026-09-14 03:15:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,2 @@
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
-  .then(()=>self.registration.unregister())
-));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.registration.unregister())));
